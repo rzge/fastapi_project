0263b391f2ae3b50517c24b9d65398d73f4dc5df
Владислав Труханов <trukhanov.vladislav@gmail.com>
2023-08-19 16:24:57 +0300

    Валидация возврата пользователю данных при het_hotels

--- app/main.py
+++ app/main.py
@@ -6,13 +6,26 @@ from pydantic import BaseModel
 app = FastAPI()
 
 
+class SHotel(BaseModel):
+    address: str
+    name: str
+    stars: int
+
+
 @app.get("/hotels")
 def get_hotels(location: str,
                date_from: date,
                date_to: date,
                stars: Optional[int] = Query(None, ge=1, le=5),
-               has_spa: Optional[bool] = None):
-    return date_from, date_to
+               has_spa: Optional[bool] = None) -> list[SHotel]:
+    hotels = [
+        {
+            "address": "Улю Гагарина",
+            "name": "pupp hotel",
+            "stars": 5
+        }
+    ]
+    return hotels
 
 
 class SBooking(BaseModel):  # Pydantic схема (поэтому S)
