352a9fcf0697a7a4723c11fbfce8bfc8d902c045
Владислав Труханов <trukhanov.vladislav@gmail.com>
2023-08-19 15:47:12 +0300

    провожу аннотацию типов, Optional говорит о том, что аргумент необязателен

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from typing import Optional
+from datetime import date
 
 app = FastAPI()
 
+
 @app.get("/hotels")
-def get_hotels():
-    return 'Отель бридж резорт 5 звёзд'
+def get_hotels(location: str,
+               date_from: date,
+               date_to: date,
+               stars: Optional[int] = None,
+               has_spa: Optional[bool] = None):
+    return date_from, date_to
