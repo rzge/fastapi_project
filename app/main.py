c1e7b37bdd1551f1dfd61ea430f1124d1826188e
Владислав Труханов <trukhanov.vladislav@gmail.com>
2023-08-19 15:49:40 +0300

    сделали ограничение на количество звёзд

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from typing import Optional
 from datetime import date
 
@@ -9,6 +9,6 @@ app = FastAPI()
 def get_hotels(location: str,
                date_from: date,
                date_to: date,
-               stars: Optional[int] = None,
+               stars: Optional[int] = Query(None, ge=1, le=5),
                has_spa: Optional[bool] = None):
     return date_from, date_to
