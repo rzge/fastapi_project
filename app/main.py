afa8b189b85f647a9c2b11e1a2ff3fd4c7ca0dc7
Владислав Труханов <trukhanov.vladislav@gmail.com>
2023-08-19 16:35:56 +0300

    Валидация передаваемых параметров get запроса через класс

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Depends
 from typing import Optional
 from datetime import date
 from pydantic import BaseModel
@@ -6,26 +6,25 @@ from pydantic import BaseModel
 app = FastAPI()
 
 
-class SHotel(BaseModel):
-    address: str
-    name: str
-    stars: int
+class HotelsSearchArgs:
+    def __init__(
+            self,
+            location: str,
+            date_from: date,
+            date_to: date,
+            stars: Optional[int] = Query(None, ge=1, le=5),
+            has_spa: Optional[bool] = None
+    ):
+        self.location = location
+        self.date_from = date_from
+        self.date_to = date_to
+        self.stars = stars
+        self.has_spa = has_spa
 
 
 @app.get("/hotels")
-def get_hotels(location: str,
-               date_from: date,
-               date_to: date,
-               stars: Optional[int] = Query(None, ge=1, le=5),
-               has_spa: Optional[bool] = None) -> list[SHotel]:
-    hotels = [
-        {
-            "address": "Улю Гагарина",
-            "name": "pupp hotel",
-            "stars": 5
-        }
-    ]
-    return hotels
+def get_hotels(search_args: HotelsSearchArgs = Depends()):
+    return search_args
 
 
 class SBooking(BaseModel):  # Pydantic схема (поэтому S)
