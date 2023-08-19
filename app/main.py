0609b1c955789f3b70eb5e98c9f0428031964d87
Владислав Труханов <trukhanov.vladislav@gmail.com>
2023-08-19 16:04:02 +0300

    создал эндпоинт для пост запроса и Pydantic схему

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Query
 from typing import Optional
 from datetime import date
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -12,3 +13,14 @@ def get_hotels(location: str,
                stars: Optional[int] = Query(None, ge=1, le=5),
                has_spa: Optional[bool] = None):
     return date_from, date_to
+
+
+class SBooking(BaseModel):  # Pydantic схема (поэтому S)
+    room_id: int
+    date_from: date
+    date_to: date
+
+
+@app.post('/bookings')
+def add_booking(booking: SBooking):
+    pass
