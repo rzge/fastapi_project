5183ad8bc435a5329c687e7d039bb95ebfb9e2d6
Владислав Труханов <trukhanov.vladislav@gmail.com>
2023-08-20 06:50:13 +0300

    исправил импорт

--- app/database.py
+++ app/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
-from config import settings
+from app.config import settings
 DB_HOST = 'localhost'
 DB_PORT = 5432
 DB_USER = 'postgres'
