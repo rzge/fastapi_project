60b03b6bbf976ff103ccdb3b5c7ec1fc022468a3
Владислав Труханов <trukhanov.vladislav@gmail.com>
2023-08-20 11:13:52 +0300

    изменил название класса

--- app/users/models.py
+++ app/users/models.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column, Integer, String, JSON, ForeignKey
 from app.database import Base
 
 
-class Rooms(Base):
+class Users(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, nullable=False)
