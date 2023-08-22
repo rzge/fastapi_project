1e0923a782cc699a00371c5a7b07a842fc157818
Владислав Труханов <trukhanov.vladislav@gmail.com>
2023-08-22 21:22:47 +0300

    наследуемся от BaseDAO + записываем в поле класса BookindDAO объект класса Bookings

--- app/bookings/dao.py
+++ app/bookings/dao.py
@@ -3,17 +3,10 @@
 from sqlalchemy import select
 
 from app.bookings.models import Bookings
+from app.dao.base import BaseDAO
 from app.database import async_session_maker
 
 
-class BookingDAO:
+class BookingDAO(BaseDAO):
+    model = Bookings
 
-    @classmethod
-    async def find_all(cls):
-        # если не использовать classmethod, то придётся
-        # писать bookings = BookingDAO(); bookings.find_all(),
-        # а с классметодом всего лишь BookingDAO.find_all()
-        async with async_session_maker() as session:
-            query = select(Bookings)
-            bookings = await session.execute(query)
-            return bookings.mappings().all()  # эту конструкцию можно вызвать только один раз!
