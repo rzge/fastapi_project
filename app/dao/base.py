32f1dac65b5b8136f3878bd3e9d4e3653f006c4a
Владислав Труханов <trukhanov.vladislav@gmail.com>
2023-08-22 21:35:42 +0300

    find_one_or_none - метод для выявления зарегистрированности пользователя

--- app/dao/base.py
+++ app/dao/base.py
@@ -7,8 +7,16 @@ class BaseDAO:
     model = None
 
     @classmethod
-    async def find_all(cls):
+    async def find_one_or_none(cls, **filter_by):
         async with async_session_maker() as session:
-            query = select(cls.model)
+            query = select(cls.model).filter_by(**filter_by)
+            result = await session.execute(query)
+            return result.scalar_one_or_none()
+
+    @classmethod
+    async def find_all(cls, **filter_by):
+        # фильтры задаём словарём (kwargs)
+        async with async_session_maker() as session:
+            query = select(cls.model).filter_by(**filter_by)
             result = await session.execute(query)
             return result.mappings().all()  # эту конструкцию можно вызвать только один раз!
