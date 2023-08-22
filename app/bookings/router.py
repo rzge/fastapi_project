5fae0845ba5f8694fb7c5e0c60c60ee97f607e22
Владислав Труханов <trukhanov.vladislav@gmail.com>
2023-08-22 21:08:14 +0300

    используем BookingDAO

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from sqlalchemy import select
 
+from app.bookings.dao import BookingDAO
 from app.bookings.models import Bookings
 from app.database import async_session_maker
 
@@ -12,7 +13,9 @@ router = APIRouter(
 
 @router.get("")  # дописывается в /bookings
 async def get_bookings():
-    async with async_session_maker() as session:
-        query = select(Bookings)  # SELECT * FROM bookings;
-        result = await session.execute(query)
-        return result.mappings().all()
+    result = await BookingDAO.find_all()
+    return result
+    # async with async_session_maker() as session:
+    #     query = select(Bookings)  # SELECT * FROM bookings;
+    #     result = await session.execute(query)
+    #     return result.mappings().all()
