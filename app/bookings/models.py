20e3bfbc5c20dc5f67ea43441cdbfe37f53f4705
Владислав Труханов <trukhanov.vladislav@gmail.com>
2023-08-22 19:56:36 +0300

    убрал Computed, так как вызывала ошибку

--- app/bookings/models.py
+++ app/bookings/models.py
@@ -7,10 +7,11 @@ class Bookings(Base):
     __tablename__ = 'bookings'
 
     id = Column(Integer, primary_key=True)
-    room_id = Column(ForeignKey("rooms.id"))
-    user_id = Column(ForeignKey("users.id"))
+    room_id = Column(ForeignKey("rooms.id"), nullable=False)
+    user_id = Column(ForeignKey("users.id"), nullable=False)
     date_from = Column(Date, nullable=False)
     date_to = Column(Date, nullable=False)
     price = Column(Integer, nullable=False)
-    total_cost = Column(Integer, Computed("(date_to - date_from) * price"))
-    total_days = Column(Integer, Computed("date_to - date_from"))
+    total_cost = Column(Integer) # проверим такую запись
+    total_days = Column(Integer)
+    # COMPUTED вызывает ошибку (а именно GENERATED ALWAYS AS)
