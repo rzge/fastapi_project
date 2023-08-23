0200e44f4e4d593acf6de83bb1a447cb9bae4b0b
Владислав Труханов <trukhanov.vladislav@gmail.com>
2023-08-23 12:12:02 +0300

    добавил роутер для представления пользователей

--- app/main.py
+++ app/main.py
@@ -3,11 +3,11 @@ from typing import Optional
 from datetime import date
 from pydantic import BaseModel
 from app.bookings.router import router as router_bookings
-
+from app.users.router import router as router_users
 app = FastAPI()
 
+app.include_router(router_users)
 app.include_router(router_bookings)
-
 
 class HotelsSearchArgs:
     def __init__(
