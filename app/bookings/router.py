1a2fc890eb094ba8a9753cf4d7a3eebea0c0ee8c
Владислав Труханов <trukhanov.vladislav@gmail.com>
2023-08-24 12:07:45 +0300

    добавили зависимости +  выводим пользователю именно его букинги

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request, Depends
 from sqlalchemy import select
 
 from app.bookings.dao import BookingDAO
 from app.bookings.models import Bookings
 from app.database import async_session_maker
+from app.users.dependecies import get_current_user
+from app.users.models import Users
 
 router = APIRouter(
     prefix="/bookings",
@@ -12,9 +14,13 @@ router = APIRouter(
 
 
 @router.get("")  # дописывается в /bookings
-async def get_bookings():
-    result = await BookingDAO.find_all()
-    return result
+async def get_bookings(user: Users = Depends(get_current_user)):
+    # print(user, type(user), user.email)
+    return await BookingDAO.find_all(user_id=user.id)
+    # return await BookingDAO.find_all()
+    # result = await BookingDAO.find_all()
+    # return result
+
     # async with async_session_maker() as session:
     #     query = select(Bookings)  # SELECT * FROM bookings;
     #     result = await session.execute(query)
