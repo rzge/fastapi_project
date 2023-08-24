12bab55deb5e665b3b102ab9b7cab06eecbff80d
Владислав Труханов <trukhanov.vladislav@gmail.com>
2023-08-24 15:17:02 +0300

    добавил правильные формат написания исключений

--- app/exceptions.py
+++ app/exceptions.py
@@ -1,10 +1,21 @@
 from fastapi import HTTPException, status
 
-UserAlreadyExistsException = HTTPException(
-    status_code=status.HTTP_409_CONFLICT,
-    detail="Пользователь уже существует"
-)
 
+# правильный стиль написания кастомных исключений
+class BookingException(HTTPException):  # <-- наследуемся от HTTPException, который наследован от Exception
+    status_code = 500  # <-- задаем значения по умолчанию
+    detail = ""
+
+    def __init__(self):
+        super().__init__(status_code=self.status_code, detail=self.detail)
+
+
+class UserAlreadyExistsException(BookingException):  # <-- обязательно наследуемся от нашего класса
+    status_code = status.HTTP_409_CONFLICT
+    detail = "Пользователь уже существует"
+
+
+# неправильный стиль написания кастомных исключений
 IncorrectEmailOrPasswordException = HTTPException(
     status_code=status.HTTP_401_UNAUTHORIZED,
     detail="Неверная почта или пароль"
@@ -24,4 +35,3 @@ IncorrectTokenFormatException = HTTPException(
     status_code=status.HTTP_401_UNAUTHORIZED,
     detail="Неверный формат токена",
 )
-
