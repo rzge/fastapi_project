c7c1f9b3bc20ab56cf2cd1aad1528403726decce
Владислав Труханов <trukhanov.vladislav@gmail.com>
2023-08-24 15:11:01 +0300

    добавлены исключения

--- app/exceptions.py
+++ app/exceptions.py
@@ -4,3 +4,24 @@ UserAlreadyExistsException = HTTPException(
     status_code=status.HTTP_409_CONFLICT,
     detail="Пользователь уже существует"
 )
+
+IncorrectEmailOrPasswordException = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Неверная почта или пароль"
+)
+
+TokenExpiredException = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Токен истёк"
+)
+
+TokenAbsentException = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Токен отсутствует"
+)
+
+IncorrectTokenFormatException = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Неверный формат токена",
+)
+
