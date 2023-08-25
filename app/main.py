afd3a4ee929ecb4ca8d281b9df0f4280eb3b683d
Владислав Труханов <trukhanov.vladislav@gmail.com>
2023-08-26 01:05:03 +0300

    добавил роутер для отображения изображений + код для загрузки изображений в наше приложение

--- app/main.py
+++ app/main.py
@@ -1,14 +1,17 @@
 from fastapi import FastAPI, Query, Depends
-from typing import Optional
-from datetime import date
-from pydantic import BaseModel
+from fastapi.staticfiles import StaticFiles
 from app.bookings.router import router as router_bookings
 from app.users.router import router as router_users
 from app.pages.router import router as router_pages
 from app.hotels.router import router as router_hotels
+from app.images.router import router as router_images
+
 app = FastAPI()
+# монтируем директорию для статических файлов
+app.mount("/static", StaticFiles(directory="app/static"), "static")
 
 app.include_router(router_users)
 app.include_router(router_bookings)
 app.include_router(router_pages)
 app.include_router(router_hotels)
+app.include_router(router_images)
