8a9606f049a23c569175edbea8857366f438d34a
Владислав Труханов <trukhanov.vladislav@gmail.com>
2023-08-26 01:41:52 +0300

    добавил настройки для общения фронтендерских фреймворков с нашим сервисом

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, Query, Depends
+from fastapi import FastAPI, Query
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 from app.bookings.router import router as router_bookings
 from app.users.router import router as router_users
 from app.pages.router import router as router_pages
@@ -15,3 +16,15 @@ app.include_router(router_bookings)
 app.include_router(router_pages)
 app.include_router(router_hotels)
 app.include_router(router_images)
+
+origins = [
+    "http://localhost:3000",
+]  # адреса сервисов, которые могут обращаться к нашему апи. Нужно для фронтендеров
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['GET', 'POST', 'OPTIONS', 'DELETE', 'PATCH', 'PUT'],
+    allow_headers=['Content-Type', "Set-Cookie", "Access-Control-Allow-Headers", "Access-Authorization"],
+)
