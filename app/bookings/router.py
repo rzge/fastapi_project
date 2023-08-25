29dd82cf2132cbfb18b327b55e80c68161dd5bb3
Владислав Труханов <trukhanov.vladislav@gmail.com>
2023-08-25 16:07:44 +0300

    добавил улучшение кода, теперь можно бронировать номер

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter, Request, Depends
+from datetime import date
+
+from fastapi import APIRouter, Request, Depends, HTTPException
 from sqlalchemy import select
 
 from app.bookings.dao import BookingDAO
@@ -28,6 +30,9 @@ async def get_bookings(user: Users = Depends(get_current_user)):
 
 @router.post("")
 async def add_booking(
+        room_id: int, date_from: date, date_to: date,
         user: Users = Depends(get_current_user)
 ):
-    await BookingDAO.add(user_id = user.id)
+    booking = await BookingDAO.add(user.id, room_id, date_from, date_to)
+    if not booking:
+        raise HTTPException(status_code=409, detail='Не осталось свободных номеров')
