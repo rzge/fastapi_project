fdfa970d3efafe6da36878b827f2dfe521ff0505
Владислав Труханов <trukhanov.vladislav@gmail.com>
2023-08-28 15:14:18 +0300

    добавлена модель броней в админке

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,5 +1,6 @@
 from sqladmin import ModelView
 
+from app.bookings.models import Bookings
 from app.users.models import Users
 
 
@@ -10,3 +11,9 @@ class UsersAdmin(ModelView, model=Users):
     name = "Пользователь"
     name_plural = "Пользователи"
     icon = "fa-solid fa-user"
+
+class BookingsAdmin(ModelView, model=Bookings):
+    column_list = [c.name for c in Bookings.__table__.c] + [Bookings.user]
+    column_details_exclude_list = [Users.hashed_password] # прячем пароль
+    name = "Бронь"
+    name_plural = "Брони"
