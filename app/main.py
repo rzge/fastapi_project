d82ffd6bc87a242dbd0753a12a5d55b568166b4a
Владислав Труханов <trukhanov.vladislav@gmail.com>
2023-08-28 14:47:26 +0300

    в main.py добавил первоначальные настройки для админки

--- app/main.py
+++ app/main.py
@@ -3,15 +3,17 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from app.bookings.router import router as router_bookings
 from app.config import settings
+from app.database import engine
+from app.users.models import Users
 from app.users.router import router as router_users
 from app.pages.router import router as router_pages
 from app.hotels.router import router as router_hotels
 from app.images.router import router as router_images
-
+from sqladmin import Admin, ModelView
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
 from fastapi_cache.decorator import cache
-
+from sqladmin import Admin, ModelView
 from redis import asyncio as aioredis
 
 app = FastAPI()
@@ -37,7 +39,17 @@ app.add_middleware(
 )
 
 
-@app.on_event("startup") # При старте предложения начинает прогонять функцию ниже
+@app.on_event("startup")  # При старте предложения начинает прогонять функцию ниже
 async def startup():
     redis = aioredis.from_url(f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}")
     FastAPICache.init(RedisBackend(redis), prefix="cache")
+
+# настраиваем админку
+admin = Admin(app, engine)
+
+
+class UsersAdmin(ModelView, model=Users):
+    column_list = [Users.id, Users.email]
+
+
+admin.add_view(UsersAdmin)
