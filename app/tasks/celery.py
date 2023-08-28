871ed65364b04acd3e8cde548c40c90c0aa46cc9
Владислав Труханов <trukhanov.vladislav@gmail.com>
2023-08-28 12:54:21 +0300

    скрыл данные

--- app/tasks/celery.py
+++ app/tasks/celery.py
@@ -1,8 +1,8 @@
 from celery import Celery
-
+from app.config import settings
 celery = Celery(
     "tasks",
-    broker="redis://localhost:6379",
+    broker=f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}",
     include=["app.tasks.tasks"]  # папка, где хранятся задачки
 )
 # команда для запуска celery -A app.tasks.celery:celery worker --loglevel=INFO --pool=solo
