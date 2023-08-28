7a5ea028db695461d74a252be6065500e80acdd1
Владислав Труханов <trukhanov.vladislav@gmail.com>
2023-08-28 17:26:56 +0300

    если тестируем код, то подключаемся к тестовой БД

--- app/database.py
+++ app/database.py
@@ -1,3 +1,4 @@
+from sqlalchemy import NullPool
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 from app.config import settings
@@ -7,8 +8,12 @@ DB_USER = 'postgres'
 DB_NAME = "postgres"
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{settings.DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_PARAMS = {}
+if settings.MODE == "TEST":
+    DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{settings.DB_PASS}@{DB_HOST}:{DB_PORT}/test_booking_db"
+    DATABASE_PARAMS = {"poolclass": NullPool}
 
-engine = create_async_engine(DATABASE_URL)
+engine = create_async_engine(DATABASE_URL, **DATABASE_PARAMS)
 
 async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
