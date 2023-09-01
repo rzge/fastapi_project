fd24c017175e54f4628b5ead21aba084a5a3f0a0
Владислав Труханов <trukhanov.vladislav@gmail.com>
2023-09-01 21:18:02 +0300

    сделал простой тест на регистрвцию пользователя

--- app/tests/unit_tests/test_users/test_api.py
+++ app/tests/unit_tests/test_users/test_api.py
@@ -1,2 +1,12 @@
-def test_abc():  # Нужен тест, чтоб функции прогнались
-    assert 1 == 1
+from httpx import AsyncClient
+import pytest
+
+# прописывай pytest -v, больше инфы станет видно в консоли
+
+async def test_register_user(ac: AsyncClient):
+    response = await ac.post("/auth/register", json={
+        "email": "kot@pes.com",
+        "password": "kotopes"
+    })
+
+    assert response.status_code == 200
