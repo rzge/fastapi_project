4ec20cb52912f497c0b7254159c03b6297cdd1aa
Владислав Труханов <trukhanov.vladislav@gmail.com>
2023-09-01 21:21:05 +0300

    при помощи parametrize, можно давать последовательность действий (попытаться зарегать уже существующего пользователя например)

--- app/tests/unit_tests/test_users/test_api.py
+++ app/tests/unit_tests/test_users/test_api.py
@@ -1,12 +1,16 @@
 from httpx import AsyncClient
 import pytest
 
-# прописывай pytest -v, больше инфы станет видно в консоли
 
-async def test_register_user(ac: AsyncClient):
+# прописывай pytest -v, больше инфы станет видно в консоли
+@pytest.mark.parametrize("email,password, status_code", [
+    ("kot@pes.com", "kotopes", 200),
+    ("kot@pes.com", "kot0pes", 409)
+])
+async def test_register_user(email, password, status_code, ac: AsyncClient):
     response = await ac.post("/auth/register", json={
-        "email": "kot@pes.com",
-        "password": "kotopes"
+        "email": email,
+        "password": password
     })
 
-    assert response.status_code == 200
+    assert response.status_code == status_code
