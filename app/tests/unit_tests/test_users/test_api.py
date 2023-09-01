fc2a16889e93ffb0422d4d47c6f2c5c25ae1e052
Владислав Труханов <trukhanov.vladislav@gmail.com>
2023-09-01 21:30:09 +0300

    добавили тест, который проверяет, может ли залогиниться пользователь

--- app/tests/unit_tests/test_users/test_api.py
+++ app/tests/unit_tests/test_users/test_api.py
@@ -15,3 +15,16 @@ async def test_register_user(email, password, status_code, ac: AsyncClient):
     })
 
     assert response.status_code == status_code
+
+
+@pytest.mark.parametrize("email,password,status_code", [
+    ('test@test.com', "test", 200),
+    ('artem@example.com', "artem", 200)
+])
+async def test_login_user(email, password, status_code, ac: AsyncClient):
+    response = await ac.post("/auth/login", json={
+        "email": email,
+        "password": password
+    })
+
+    assert response.status_code == status_code
