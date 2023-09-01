db412e3ccc127851fd3bfc66ffbd2126ccbf5b58
Владислав Труханов <trukhanov.vladislav@gmail.com>
2023-09-01 21:24:41 +0300

    добавил тест на валидацию email

--- app/tests/unit_tests/test_users/test_api.py
+++ app/tests/unit_tests/test_users/test_api.py
@@ -5,7 +5,8 @@ import pytest
 # прописывай pytest -v, больше инфы станет видно в консоли
 @pytest.mark.parametrize("email,password, status_code", [
     ("kot@pes.com", "kotopes", 200),
-    ("kot@pes.com", "kot0pes", 409)
+    ("kot@pes.com", "kot0pes", 409),
+    ("abcde", "pesokot", 422)
 ])
 async def test_register_user(email, password, status_code, ac: AsyncClient):
     response = await ac.post("/auth/register", json={
