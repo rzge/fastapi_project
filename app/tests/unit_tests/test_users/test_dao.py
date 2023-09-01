b88cb57ad1ac5a1b5f8dc63123f12b4c3bf2d29a
Владислав Труханов <trukhanov.vladislav@gmail.com>
2023-09-01 21:42:52 +0300

    проверяем, что обращения к бд правильно работают

--- app/tests/unit_tests/test_users/test_dao.py
+++ app/tests/unit_tests/test_users/test_dao.py
@@ -1,7 +1,18 @@
+import pytest
+
 from app.users.dao import UsersDAO
 
-async def test_find_user_by_id():
-    user = await UsersDAO.find_by_id(1)
 
-    assert user.email == 'test@test.com'
-    assert user.id == 1
+@pytest.mark.parametrize("user_id,email,exists", [
+    (1, "test@test.com", True),
+    (2, "artem@example.com", True),
+    (23232, "kakaya-to fignya", False)
+])
+async def test_find_user_by_id(user_id, email, exists):
+    user = await UsersDAO.find_by_id(user_id)
+    if exists:
+        assert user
+        assert user.email == email
+        assert user.id == user_id
+    else:
+        assert not user
