390ad95b8cc5c8fd3d38454141f98aca6d9c9d51
Владислав Труханов <trukhanov.vladislav@gmail.com>
2023-09-03 15:51:10 +0300

    при помощи isort отсортировл импорты

--- app/main.py
+++ app/main.py
@@ -1,23 +1,22 @@
 from fastapi import FastAPI, Query
-from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
-
-from app.admin.auth import authentication_backend
-from app.admin.views import UsersAdmin, BookingsAdmin
-from app.bookings.router import router as router_bookings
-from app.config import settings
-from app.database import engine
-from app.users.models import Users
-from app.users.router import router as router_users
-from app.pages.router import router as router_pages
-from app.hotels.router import router as router_hotels
-from app.images.router import router as router_images
-from sqladmin import Admin, ModelView
+from fastapi.staticfiles import StaticFiles
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
 from fastapi_cache.decorator import cache
-from sqladmin import Admin, ModelView
 from redis import asyncio as aioredis
+from sqladmin import Admin, ModelView
+
+from app.admin.auth import authentication_backend
+from app.admin.views import BookingsAdmin, UsersAdmin
+from app.bookings.router import router as router_bookings
+from app.config import settings
+from app.database import engine
+from app.hotels.router import router as router_hotels
+from app.images.router import router as router_images
+from app.pages.router import router as router_pages
+from app.users.models import Users
+from app.users.router import router as router_users
 
 app = FastAPI()
 # монтируем директорию для статических файлов
