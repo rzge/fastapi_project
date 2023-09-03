4cee0b7b392b3018a94ed3381b5b081e0983040a
Владислав Труханов <trukhanov.vladislav@gmail.com>
2023-09-03 16:51:02 +0300

    добавил обработку ошибку добавления пользователя в БД и логгируем

--- app/bookings/dao.py
+++ app/bookings/dao.py
@@ -8,8 +8,8 @@ from app.bookings.models import Bookings
 from app.dao.base import BaseDAO
 from app.database import async_session_maker
 from app.rooms.models import Rooms
-
-
+from sqlalchemy.exc import SQLAlchemyError
+from app.logger import logger
 class BookingDAO(BaseDAO):
     model = Bookings
 
@@ -21,61 +21,72 @@ class BookingDAO(BaseDAO):
         date_from: date,
         date_to: date,
     ):
-        async with async_session_maker() as session:
-            booked_rooms = (
-                select(Bookings)
-                .where(
-                    and_(
-                        Bookings.room_id == 1,
-                        or_(
-                            and_(
-                                Bookings.date_from >= date_from,
-                                Bookings.date_from <= date_to,
+        try:
+            async with async_session_maker() as session:
+                booked_rooms = (
+                    select(Bookings)
+                    .where(
+                        and_(
+                            Bookings.room_id == 1,
+                            or_(
+                                and_(
+                                    Bookings.date_from >= date_from,
+                                    Bookings.date_from <= date_to,
+                                ),
+                                and_(
+                                    Bookings.date_from <= date_from,
+                                    Bookings.date_to > date_from,
+                                ),
                             ),
-                            and_(
-                                Bookings.date_from <= date_from,
-                                Bookings.date_to > date_from,
-                            ),
-                        ),
+                        )
                     )
+                    .cte("booked_rooms")
                 )
-                .cte("booked_rooms")
-            )
 
-            get_rooms_left = (
-                select(
-                    (Rooms.quantity - func.count(booked_rooms.c.room_id)).label(
-                        "rooms_left"
+                get_rooms_left = (
+                    select(
+                        (Rooms.quantity - func.count(booked_rooms.c.room_id)).label(
+                            "rooms_left"
+                        )
                     )
+                    .select_from(Rooms)
+                    .join(booked_rooms, booked_rooms.c.room_id == Rooms.id, isouter=True)
+                    .where(Rooms.id == 1)
+                    .group_by(Rooms.quantity, booked_rooms.c.room_id)
                 )
-                .select_from(Rooms)
-                .join(booked_rooms, booked_rooms.c.room_id == Rooms.id, isouter=True)
-                .where(Rooms.id == 1)
-                .group_by(Rooms.quantity, booked_rooms.c.room_id)
+                rooms_left = await session.execute(get_rooms_left)
+                rooms_left: int = rooms_left.scalar()
+                if rooms_left > 0:
+                    get_price = select(Rooms.price).filter_by(id=room_id)
+                    price = await session.execute(get_price)
+                    price: int = price.scalar()
+                    add_booking = (
+                        insert(Bookings)
+                        .values(
+                            room_id=room_id,
+                            user_id=user_id,
+                            date_from=date_from,
+                            date_to=date_to,
+                            price=price,
+                        )
+                        .returning(Bookings)
+                    )
+                    new_booking = await session.execute(add_booking)
+                    await session.commit()
+                    return new_booking.scalar()
+                else:
+                    return None
+        except (SQLAlchemyError, Exception) as e:
+            if isinstance(e, SQLAlchemyError):
+                msg = "Database exc: Cannot add booking"
+            elif isinstance(e, Exception):
+                msg = "Exc: Unknown error"
+            extra = {"user_id": user_id,
+                     "room_id": room_id
+                     }
+            logger.error(
+                msg, extra=extra, exc_info=True
             )
-            rooms_left = await session.execute(get_rooms_left)
-            rooms_left: int = rooms_left.scalar()
-            if rooms_left > 0:
-                get_price = select(Rooms.price).filter_by(id=room_id)
-                price = await session.execute(get_price)
-                price: int = price.scalar()
-                add_booking = (
-                    insert(Bookings)
-                    .values(
-                        room_id=room_id,
-                        user_id=user_id,
-                        date_from=date_from,
-                        date_to=date_to,
-                        price=price,
-                    )
-                    .returning(Bookings)
-                )
-                new_booking = await session.execute(add_booking)
-                await session.commit()
-                return new_booking.scalar()
-            else:
-                return None
-
     @classmethod
     async def delete(cls, booking_id: int, user_id: int):
         async with async_session_maker() as session:
